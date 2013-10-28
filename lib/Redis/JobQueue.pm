1e8ed63e0779ff70a4bb33adae7525ad61c10305
Sergey Gladkov <sgladkov@trackingsoft.com>
2013-10-28 20:47:30 +0200

    This distribution have a list of provided modules
    - Changes Changes.
    Change-Id: Id95cf6e9a2f30a6a03be177871709f43494e430a

--- lib/Redis/JobQueue.pm
+++ lib/Redis/JobQueue.pm
@@ -6,7 +6,7 @@ Redis::JobQueue - Job queue management implemented using Redis server.
 
 =head1 VERSION
 
-This documentation refers to C<Redis::JobQueue> version 1.06
+This documentation refers to C<Redis::JobQueue> version 1.07
 
 =cut
 
@@ -18,7 +18,7 @@ use warnings;
 
 # ENVIRONMENT ------------------------------------------------------------------
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 use Exporter qw(
     import
--- lib/Redis/JobQueue/Job.pm
+++ lib/Redis/JobQueue/Job.pm
@@ -6,7 +6,7 @@ Redis::JobQueue::Job - Object interface for creating and manipulating jobs
 
 =head1 VERSION
 
-This documentation refers to C<Redis::JobQueue::Job> version 1.06
+This documentation refers to C<Redis::JobQueue::Job> version 1.07
 
 =cut
 
@@ -18,7 +18,7 @@ use warnings;
 
 # ENVIRONMENT ------------------------------------------------------------------
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 #-- load the modules -----------------------------------------------------------
 
